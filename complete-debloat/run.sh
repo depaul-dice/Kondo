9d6c1fbfa4c67f4efb58d4623756ba6c85253d65
Aniket Modi <aniketmodi.iitd@gmaiil.com>
2023-08-12 06:07:21 -0500

    minor fixes in run.sh

--- complete-debloat/run.sh
+++ complete-debloat/run.sh
@@ -12,7 +12,7 @@ sudo apt install -y build-essential
 
 echo -e "\n\n##########################"
 echo -e "##########Installing AFL######"
-echo -e "\n\n##########################"
+echo -e "##########################\n\n"
 git clone https://github.com/google/AFL.git
 cd AFL
 sudo make
@@ -21,14 +21,14 @@ cd ..
 
 echo -e "\n\n##########################"
 echo -e "Compiling source programs"
-echo -e "\n\n##########################"
+echo -e "##########################\n\n"
 make
 # compile space variation programs
 bash space_variation_exp/compile.sh
 
 echo -e "\n\n##########################"
 echo -e "\nCompiling AFL Programs"
-echo -e "\n\n##########################"
+echo -e "##########################\n\n"
 bash AFL-experiments/compile.sh          # TODO: UNCOMMENT
 
 mkdir -p logs
@@ -36,37 +36,37 @@ mkdir -p data
 
 echo -e "\n\n##########################"
 echo -e "\nRunning Kondo and BF"
-echo -e "\n\n##########################"
+echo -e "##########################\n\n"
 python3 src/run.py
 
 echo -e "\n\n##########################"
 echo -e "\nSleeping to wait for AFL compilation"
-echo -e "\n\n!!!!Please increase the sleep time if AFL compilation is turned on above. It usually takes a few hours."
-echo -e "\n\n##########################"
+echo -e "!!!!Please increase the sleep time if AFL compilation is turned on above. It usually takes a few hours."
+echo -e "##########################\n\n"
 sleep 60 # 1 min or until compilation of afl programs end
 
 echo -e "\n\n##########################"
 echo -e "\nRunning AFL"
-echo -e "\n\n##########################"
+echo -e "##########################\n\n"
 bash AFL-experiments/fuzz.sh
 
 echo -e "\n\n##########################"
 echo -e "\nRunning offset space variation experiments"
-echo -e "\n\n##########################"
+echo -e "##########################\n\n"
 python3 offset_space_exp.py
 
 echo -e "\n\n##########################"
 echo -e "\nComputing statistics from the generated data"
-echo -e "\n\n##########################"
+echo -e "##########################\n\n"
 python3 compute_stats.py
 
 echo -e "\n\n##########################"
 echo -e "\nRunning Kondo with varied center hull distance threshold"
-echo -e "\n\n##########################"
+echo -e "##########################\n\n"
 python3 vary_hull_thresh.py
 
 echo -e "\n\n##########################"
 echo -e "\nPlotting"
-echo -e "\n\n##########################"
+echo -e "##########################\n\n"
 python3 plot_stats.py
                                                                                                                                                                                           71,1          Bot
