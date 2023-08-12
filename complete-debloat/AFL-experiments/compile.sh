151262ecfb3abbdd8e47dd86e60472b98f542558
Aniket Modi <aniketmodi.iitd@gmaiil.com>
2023-08-12 06:04:10 -0500

    fix compile.sh

--- complete-debloat/AFL-experiments/compile.sh
+++ complete-debloat/AFL-experiments/compile.sh
@@ -1,8 +1,8 @@
-afl-clang ./CS1/CS1.c ./CS1/offset_check.c -o ./CS1/CS1 &
-afl-clang ./CS2/CS2.c ./CS2/offset_check.c -o ./CS2/CS2 &
-afl-clang ./CS3/CS3.c ./CS3/offset_check.c -o ./CS3/CS3 &
-afl-clang ./CS4/CS4.c ./CS4/offset_check.c -o ./CS4/CS4 &
-afl-clang ./CS5/CS5.c ./CS5/offset_check.c -o ./CS5/CS5 &
-afl-clang ./PRL2D/PRL2D.c ./PRL2D/offset_check.c -o ./PRL2D/PRL2D &
-afl-clang ./LDC2D/LDC2D.c ./LDC2D/offset_check.c -o ./LDC2D/LDC2D &
-afl-clang ./RDC2D/RDC2D.c ./RDC2D/offset_check.c -o ./RDC2D/RDC2D
+afl-clang AFL-experiments/CS1/CS1.c AFL-experiments/CS1/offset_check.c -o AFL-experiments/CS1/CS1 &
+afl-clang AFL-experiments/CS2/CS2.c AFL-experiments/CS2/offset_check.c -o AFL-experiments/CS2/CS2 &
+afl-clang AFL-experiments/CS3/CS3.c AFL-experiments/CS3/offset_check.c -o AFL-experiments/CS3/CS3 &
+afl-clang AFL-experiments/CS4/CS4.c AFL-experiments/CS4/offset_check.c -o AFL-experiments/CS4/CS4 &
+afl-clang AFL-experiments/CS5/CS5.c AFL-experiments/CS5/offset_check.c -o AFL-experiments/CS5/CS5 &
+afl-clang AFL-experiments/PRL2D/PRL2D.c AFL-experiments/PRL2D/offset_check.c -o AFL-experiments/PRL2D/PRL2D &
+afl-clang AFL-experiments/LDC2D/LDC2D.c AFL-experiments/LDC2D/offset_check.c -o AFL-experiments/LDC2D/LDC2D &
+afl-clang AFL-experiments/RDC2D/RDC2D.c AFL-experiments/RDC2D/offset_check.c -o AFL-experiments/RDC2D/RDC2D
--- complete-debloat/run.sh
+++ complete-debloat/run.sh
@@ -1,4 +1,4 @@
-echo "Installing python"
+echo -e "Installing python"
 sudo apt-get -y install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get -y update
@@ -10,63 +10,63 @@ sudo apt -y install clang lldb lld
 # install gcc etc
 sudo apt install -y build-essential
 
-echo "\n\n##########################"
-echo "##########Installing AFL######"
-echo "\n\n##########################"
+echo -e "\n\n##########################"
+echo -e "##########Installing AFL######"
+echo -e "\n\n##########################"
 git clone https://github.com/google/AFL.git
 cd AFL
 sudo make
 sudo make install
 cd ..
 
-echo "\n\n##########################"
-echo "Compiling source programs"
-echo "\n\n##########################"
+echo -e "\n\n##########################"
+echo -e "Compiling source programs"
+echo -e "\n\n##########################"
 make
 # compile space variation programs
 bash space_variation_exp/compile.sh
 
-echo "\n\n##########################"
-echo "\nCompiling AFL Programs"
-echo "\n\n##########################"
+echo -e "\n\n##########################"
+echo -e "\nCompiling AFL Programs"
+echo -e "\n\n##########################"
 bash AFL-experiments/compile.sh          # TODO: UNCOMMENT
 
 mkdir -p logs
 mkdir -p data
 
-echo "\n\n##########################"
-echo "\nRunning Kondo and BF"
-echo "\n\n##########################"
+echo -e "\n\n##########################"
+echo -e "\nRunning Kondo and BF"
+echo -e "\n\n##########################"
 python3 src/run.py
 
-echo "\n\n##########################"
-echo "\nSleeping to wait for AFL compilation"
-echo "\n\n!!!!Please increase the sleep time if AFL compilation is turned on above. It usually takes a few hours."
-echo "\n\n##########################"
+echo -e "\n\n##########################"
+echo -e "\nSleeping to wait for AFL compilation"
+echo -e "\n\n!!!!Please increase the sleep time if AFL compilation is turned on above. It usually takes a few hours."
+echo -e "\n\n##########################"
 sleep 60 # 1 min or until compilation of afl programs end
 
-echo "\n\n##########################"
-echo "\nRunning AFL"
-echo "\n\n##########################"
+echo -e "\n\n##########################"
+echo -e "\nRunning AFL"
+echo -e "\n\n##########################"
 bash AFL-experiments/fuzz.sh
 
-echo "\n\n##########################"
-echo "\nRunning offset space variation experiments"
-echo "\n\n##########################"
+echo -e "\n\n##########################"
+echo -e "\nRunning offset space variation experiments"
+echo -e "\n\n##########################"
 python3 offset_space_exp.py
 
-echo "\n\n##########################"
-echo "\nComputing statistics from the generated data"
-echo "\n\n##########################"
+echo -e "\n\n##########################"
+echo -e "\nComputing statistics from the generated data"
+echo -e "\n\n##########################"
 python3 compute_stats.py
 
-echo "\n\n##########################"
-echo "\nRunning Kondo with varied center hull distance threshold"
-echo "\n\n##########################"
+echo -e "\n\n##########################"
+echo -e "\nRunning Kondo with varied center hull distance threshold"
+echo -e "\n\n##########################"
 python3 vary_hull_thresh.py
 
-echo "\n\n##########################"
-echo "\nPlotting"
-echo "\n\n##########################"
+echo -e "\n\n##########################"
+echo -e "\nPlotting"
+echo -e "\n\n##########################"
 python3 plot_stats.py
                                                                                                                                                                                           71,1          Bot
